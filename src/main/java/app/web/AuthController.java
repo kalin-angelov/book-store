298202a3deab9778ed7c48a9f54df741295f0dde
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-10-01 23:54:13 +0300

    create: create auth controller

---
+++ src/main/java/app/web/AuthController.java
@@ -0,0 +1,33 @@
+package app.web;
+
+import app.user.service.UserService;
+import app.web.dto.RegisterRequest;
+import app.web.dto.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> registerUser(@RequestBody RegisterRequest request) {
+
+        userService.registerUser(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(Response.builder()
+                        .message("User successfully register")
+                        .build());
+
+    }
+}
