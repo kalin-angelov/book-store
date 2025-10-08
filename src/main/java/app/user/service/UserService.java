5df2bd7fe2e086e72c132332c7106d0b19e1f388
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-10-09 00:15:23 +0300

    edit: add method for verifying user credentials

--- src/main/java/app/user/service/UserService.java
+++ src/main/java/app/user/service/UserService.java
@@ -2,15 +2,20 @@ package app.user.service;
 
 import app.exeptions.EmailAlreadyExistInExceptionDB;
 import app.exeptions.PasswordException;
+import app.exeptions.UserException;
 import app.user.model.User;
 import app.user.model.UserRole;
 import app.user.repository.UserRepository;
+import app.web.dto.LoginRequest;
 import app.web.dto.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Slf4j
@@ -21,6 +26,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public void registerUser(RegisterRequest request) {
 
         if (!request.getPassword().equals(request.getConfigPassword())) {
@@ -40,12 +46,29 @@ public class UserService {
         log.info("User successfully added in DB");
     }
 
+    public void verify(LoginRequest loginRequest) {
+
+        try {
+            userRepository.findUserByEmail(loginRequest.getEmail())
+                    .orElseThrow(() -> new UsernameNotFoundException("User with the provided email [%s] is found.".formatted(loginRequest.getEmail())));
+
+        } catch (Exception exception) {
+            log.info(exception.getMessage());
+        }
+
+        throw new UserException("Invalid Email or Password");
+
+    }
+
     private User initializeUser(RegisterRequest request) {
 
         return User.builder()
                 .role(UserRole.USER)
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
+                .isActive(true)
+                .registerOn(LocalDateTime.now())
+                .updatedOn(LocalDateTime.now())
                 .build();
     }
 }
