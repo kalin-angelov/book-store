cd1ede1640e6fb2cae9d87fe2b144a7c646c2764
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-10-15 22:20:53 +0300

    edit: add e custom query to get the las 10 added books

--- src/main/java/app/book/repository/BookRepository.java
+++ src/main/java/app/book/repository/BookRepository.java
@@ -2,10 +2,18 @@ package app.book.repository;
 
 import app.book.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    @Query(
+        value = "SELECT * FROM book ORDER BY added_on DESC LIMIT 10",
+        nativeQuery = true
+    )
+    List<Book> findLatestAddedBooks();
 }
