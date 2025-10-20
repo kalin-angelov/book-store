e883747e9927946f6be5637b382717a762ccd8aa
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-10-20 10:50:31 +0300

    edit: add endpoint for password change

--- src/main/java/app/web/UserController.java
+++ src/main/java/app/web/UserController.java
@@ -2,8 +2,12 @@ package app.web;
 
 import app.user.model.User;
 import app.user.service.UserService;
+import app.web.dto.ChangePasswordRequest;
 import app.web.dto.EditUserRequest;
+import app.web.dto.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -19,5 +23,17 @@ public class UserController {
     public User editUser(@RequestParam(name = "userId")UUID userId, @RequestBody EditUserRequest request) {
 
         return userService.editUser(userId, request);
+    }
+
+    @PutMapping("/edit-password")
+    public ResponseEntity<Response> changePassword(@RequestParam(name = "userId") UUID userId, @RequestBody ChangePasswordRequest request) {
+
+        userService.editPassword(userId, request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(Response.builder()
+                        .message("Password successfully change")
+                        .build());
     }
 }
