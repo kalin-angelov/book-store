3191b46145ed048bd6eeef144bfaf6ea0552e851
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-10-26 14:42:39 +0200

    edit: handler for the validation annotations

--- src/main/java/app/web/ExceptionAdvice.java
+++ src/main/java/app/web/ExceptionAdvice.java
@@ -4,6 +4,7 @@ import app.exeptions.*;
 import app.web.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -55,6 +56,16 @@ public class ExceptionAdvice {
     public ResponseEntity<ErrorResponse> handleUserException(UserException exception) {
 
         ErrorResponse response = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException exception) {
+
+        ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getBindingResult().getFieldError().getDefaultMessage());
 
         return ResponseEntity
                 .status(response.getStatus())
