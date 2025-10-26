ff67ee5be417119ad724025aa4a19c502b73b332
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-10-26 14:39:02 +0200

    create: add test for authorService

---
+++ src/test/java/app/author/AuthorServiceTest.java
@@ -0,0 +1,47 @@
+package app.author;
+
+import app.author.model.Author;
+import app.author.repository.AuthorRepository;
+import app.author.service.AuthorService;
+import app.exeptions.AuthorException;
+import app.web.dto.AddAuthorRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static app.TestBuilder.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthorServiceTest {
+
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @InjectMocks
+    private AuthorService authorService;
+
+    @Test
+    void givenAuthorNameThatIsAlreadyInDB_whenAddAuthor_thenExceptionIsThrown() {
+
+        Author author = Author.builder()
+                .name("authorName")
+                .bio("authorBio")
+                .build();
+
+        AddAuthorRequest request = AddAuthorRequest.builder()
+                .name("authorName")
+                .build();
+
+        when(authorRepository.findAuthorByName(request.getName())).thenReturn(Optional.of(author));
+
+        assertThrows(AuthorException.class, () -> authorService.addAuthor(request));
+        verify(authorRepository, never()).save(any(Author.class));
+    }
+}
