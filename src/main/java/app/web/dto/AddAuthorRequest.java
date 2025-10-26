43be7b1fcacb483589f860760af5c7508d991d91
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-10-26 14:41:28 +0200

    edit: add some validation to the author dto fields

--- src/main/java/app/web/dto/AddAuthorRequest.java
+++ src/main/java/app/web/dto/AddAuthorRequest.java
@@ -1,5 +1,7 @@
 package app.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +9,11 @@ import lombok.Data;
 @Builder
 public class AddAuthorRequest {
 
+    @NotNull(message = "Field can't be null")
+    @NotBlank(message = "All fields are required")
     private String name;
+
+    @NotNull(message = "Field can't be null")
+    @NotBlank(message = "All fields are required")
     private String bio;
 }
